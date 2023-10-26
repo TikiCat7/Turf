4aa948306ce5962c1637cf00575031982e1a23b2
TikiCat <watarukay@gmail.com>
2023-10-26 21:59:34 +0800

    dark theme for sign-up

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,18 @@
 import { SignUp } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center h-screen">
-      <SignUp />
+      <SignUp
+        appearance={{
+          baseTheme: dark,
+          elements: {
+            formButtonPrimary:
+              "bg-primary hover:bg-primary/50 text-sm normal-case",
+          },
+        }}
+      />
     </div>
   );
 }
