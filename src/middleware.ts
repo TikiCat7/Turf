5e89a11d0b754b671c5fb6093f59745be9162cd9
TikiCat <watarukay@gmail.com>
2023-11-09 16:52:33 +0900

    Fix invitation redirect issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,21 @@
 import { authMiddleware } from '@clerk/nextjs'
+import { NextResponse } from 'next/server'
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
   publicRoutes: ['/', '/api/webhooks', '/api/webhooks/clerk'],
+
+  async afterAuth(_, req) {
+    if (
+      req.nextUrl.pathname.includes('usercreated') ||
+      req.nextUrl.pathname.includes('orgcreated')
+    ) {
+      // continue to next middleware
+      return NextResponse.next()
+    }
+  },
   // afterAuth(auth, req, evt) {
   //   if (
   //     auth.userId &&
