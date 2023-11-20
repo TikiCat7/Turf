2e9668ce09c01103206e3867a10e06ba07c48d70
TikiCat <watarukay@gmail.com>
2023-11-20 14:36:04 +0800

    Update mux-player UI

--- src/components/mux-player.tsx
+++ src/components/mux-player.tsx
@@ -2,6 +2,7 @@
 
 import type MuxPlayerElement from '@mux/mux-player'
 import MuxPlayer from '@mux/mux-player-react'
+import { DownloadIcon } from 'lucide-react'
 import { useState } from 'react'
 
 import CuepointList from '@/components/cuepoint-list'
@@ -10,6 +11,8 @@ import CuepointTag from '@/components/cuepoint-tag'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { MuxCuePointData } from '@/components/video-section'
 import { cn } from '@/lib/utils'
+
+import { Button } from './ui/button'
 // import { cuepoints } from '@/lib/db/schema'
 
 export const dynamic = 'force-dynamic'
@@ -31,6 +34,8 @@ export default function VideoPlayer({
     MuxCuePointData | undefined
   >()
 
+  const [renderTimeline, setRenderTimeline] = useState(false)
+
   return (
     <div className="grid grid-cols-10 gap-4">
       <div className="lg:col-span-7 col-span-10">
@@ -40,7 +45,7 @@ export default function VideoPlayer({
           streamType="on-demand"
           playbackId={playbackId}
           currentTime={playerRef.current?.currentTime}
-          accent-color="#ea580c"
+          accent-color="#44997F"
           // TODO: what is this for
           metadata={{
             video_id: 'video-id-54321',
@@ -50,67 +55,83 @@ export default function VideoPlayer({
           onLoadedMetadata={({ target }) => {
             const playerEl = target as MuxPlayerElement
             playerEl.addCuePoints(muxCuepoints)
+            setRenderTimeline(true)
           }}
           onCuePointChange={({ detail }: { detail: MuxCuePointData }) => {
             setActiveCuePoint(detail)
           }}
         />
-        <div className="w-full bg-white/30 h-4 rounded-md flex items-center justify-start bottom-[75px] relative">
-          {muxCuepoints.map((cuepoint) => {
-            const left = (cuepoint.time / duration) * 100
-            const isActive = cuepoint.time === activeCuePoint?.time
-            return (
-              <div
-                key={cuepoint.value.id}
-                className="relative left-0"
-                style={{
-                  color: 'bg-slate-900/10',
-                  left: `calc(${left}% - 5px)`,
-                }}
-              >
+        <div className="flex items-center space-x-2">
+          <Button
+            variant="outline"
+            onClick={() => setRenderTimeline(!renderTimeline)}
+          >
+            Timeline
+          </Button>
+          <Button variant="outline">
+            <DownloadIcon className="h-4 w-4" />
+          </Button>
+        </div>
+        {renderTimeline && (
+          <div className="w-full bg-white/30 h-3 flex items-center justify-start bottom-[110px] relative">
+            {muxCuepoints.map((cuepoint) => {
+              const left = (cuepoint.time / duration) * 100
+              const isActive = cuepoint.time === activeCuePoint?.time
+              return (
                 <div
-                  className={`${
-                    cuepoint.time === activeCuePoint?.time && ''
-                  } cursor-pointer`}
-                  onClick={() => {
-                    if (playerRef.current) {
-                      playerRef.current.currentTime = cuepoint.time
-                      if (playerRef.current.paused) playerRef.current.play()
-                    }
+                  key={cuepoint.value.id}
+                  className="relative left-0"
+                  style={{
+                    color: 'bg-slate-900/10',
+                    left: `calc(${left}% - 5px)`,
                   }}
                 >
                   <div
-                    className={cn(
-                      'bg-black/50 w-2 h-2 rounded',
-                      isActive && 'bg-black'
-                    )}
-                  />
-                  <div
-                    className={cn(
-                      'hover:opacity-100 opacity-0 left-[-65px] bottom-[12px] absolute flex transition-all duration-200 w-[130px] h-[50px] bg-slate-900/40 rounded-md translate-y-0 hover:translate-y-[-8px] flex-col items-center justify-center',
-                      isActive && 'opacity-100 translate-y-[-8px]'
-                    )}
+                    className={`${
+                      cuepoint.time === activeCuePoint?.time && ''
+                    } cursor-pointer`}
+                    onClick={() => {
+                      if (playerRef.current) {
+                        playerRef.current.currentTime = cuepoint.time
+                        if (playerRef.current.paused) playerRef.current.play()
+                      }
+                    }}
                   >
-                    <p className="font-bold text-white">
-                      ⚽️ {cuepoint.value.playCategory}
-                    </p>
-                    <p className="text-white font-xs">
-                      {new Date(cuepoint.time * 1000)
-                        .toISOString()
-                        .slice(14, 22)}
-                    </p>
+                    <div
+                      className={cn(
+                        'bg-black/50 w-2 h-2 rounded',
+                        isActive && 'bg-black'
+                      )}
+                    />
+                    <div
+                      className={cn(
+                        'hover:opacity-100 opacity-0 left-[-65px] bottom-[12px] absolute flex transition-all duration-200 w-[130px] h-[50px] bg-slate-900/40 rounded-md translate-y-0 hover:translate-y-[-8px] flex-col items-center justify-center',
+                        isActive && 'opacity-100 translate-y-[-8px]'
+                      )}
+                    >
+                      <p className="font-bold text-white">
+                        ‚öΩÔ∏è {cuepoint.value.playCategory}
+                      </p>
+                      <p className="text-white font-xs">
+                        {new Date(cuepoint.time * 1000)
+                          .toISOString()
+                          .slice(14, 22)}
+                      </p>
+                    </div>
                   </div>
+                  {/* <DeleteCuepoint id={cuepoint.value.id} /> */}
                 </div>
-                {/* <DeleteCuepoint id={cuepoint.value.id} /> */}
-              </div>
-            )
-          })}
-        </div>
+              )
+            })}
+          </div>
+        )}
       </div>
       <div className="col-span-10 lg:col-span-3">
         <Tabs defaultValue="events">
           <TabsList className="grid w-full grid-cols-2">
-            <TabsTrigger value="events">Events</TabsTrigger>
+            <TabsTrigger value="events">
+              {muxCuepoints.length} Events
+            </TabsTrigger>
             <TabsTrigger value="tag">Tag Events</TabsTrigger>
           </TabsList>
           <TabsContent value="events">
