fa7e550fdc3c20be98c851ab5f32417aa566ee3d
TikiCat <watarukay@gmail.com>
2023-11-20 12:06:59 +0800

    Fix uploader logic for /assets

--- src/app/assets/page.tsx
+++ src/app/assets/page.tsx
@@ -1,4 +1,4 @@
-import { auth, currentUser } from '@clerk/nextjs'
+import { auth } from '@clerk/nextjs'
 import { DotsHorizontalIcon } from '@radix-ui/react-icons'
 import { eq } from 'drizzle-orm'
 import {
@@ -23,7 +23,7 @@ import {
 } from '@/components/ui/dropdown-menu'
 import { Input } from '@/components/ui/input'
 import { db } from '@/lib/db'
-import { SelectVideo, users } from '@/lib/db/schema'
+import { users } from '@/lib/db/schema'
 
 export const dynamic = 'force-dynamic'
 
@@ -44,17 +44,22 @@ async function getAssets() {
   if (isPersonal) {
     return await db.query.videos.findMany({
       where: (videos, { eq }) => eq(videos.clerkUserId, clerkUser.userId!),
+      with: {
+        uploader: true,
+      },
     })
   }
 
   return await db.query.videos.findMany({
     where: (videos, { eq }) => eq(videos.clerkTeamId, clerkUser.orgId!),
+    with: {
+      uploader: true,
+    },
   })
 }
 
 export default async function Assets() {
   const allAssets = await getAssets()
-  const user = await currentUser()
   return (
     <div className="flex-col py-16 w-4/5 space-y-4">
       <div className="text-4xl font-bold mb-4">Video Gallery</div>
@@ -62,14 +67,93 @@ export default async function Assets() {
       <div className="flex flex-col space-y-4">
         {allAssets
           .sort((a, b) => (a.createdAt > b.createdAt ? -1 : 1))
-          .map((video: SelectVideo) => {
+          .map((video) => {
             return (
-              <VideoRow
-                key={video.id}
-                video={video}
-                userImageUrl={user?.imageUrl}
-                userName={user?.firstName ?? ''}
-              />
+              <div className="flex space-x-2 cursor-pointer">
+                <Link
+                  href={`assets/${video.assetId}`}
+                  className="flex flex-col md:flex-row md:space-x-2"
+                >
+                  {video.videoStatus === 'ready' ? (
+                    <div className="relative">
+                      <Image
+                        priority={true}
+                        alt="video_thumbnail"
+                        src={`https://image.mux.com/${video.playbackUrl}/thumbnail.jpg?width=300&fit_mode=pad`}
+                        className="flex rounded-md"
+                        width={279}
+                        height={157}
+                      />
+                      <Badge className="font-bold text-xs absolute bottom-2 right-2 bg-primary/80">
+                        {new Date(video.duration! * 1000)
+                          .toISOString()
+                          .slice(11, 19)}
+                      </Badge>
+                      <Badge className="absolute top-2 right-2 bg-primary/80">
+                        {video.videoTypeEnum}
+                      </Badge>
+                    </div>
+                  ) : (
+                    <div className="bg-gray-400 w-[279px] h-[157px] rounded-md" />
+                  )}
+                  <div className="flex flex-col md:w-[400px]">
+                    <div className="flex space-x-4 items-center justify-between">
+                      <p className="lg:text-xl font-semibold">
+                        {video.videoName}
+                      </p>
+                      <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                          <Button variant="ghost">
+                            <span className="sr-only">Actions</span>
+                            <DotsHorizontalIcon className="h-4 w-4" />
+                          </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                          <DropdownMenuItem className="space-x-2">
+                            <ShareIcon className="w-4 h-4" />
+                            <p>Share</p>
+                          </DropdownMenuItem>
+                          <DropdownMenuItem className="space-x-2">
+                            <DownloadIcon className="h4 w-4" />
+                            <p>Download Video</p>
+                          </DropdownMenuItem>
+                          <DropdownMenuItem className="space-x-2">
+                            <ScissorsIcon className="h-4 w-4" />
+                            <p>Clip</p>
+                          </DropdownMenuItem>
+                          <DropdownMenuItem className="space-x-2">
+                            <BookOpenIcon className="h-4 w-4" />
+                            <p>Match Info</p>
+                          </DropdownMenuItem>
+                        </DropdownMenuContent>
+                      </DropdownMenu>
+                    </div>
+                    <div className="text-muted-foreground">
+                      <TimeAgoClient date={video.createdAt} locale="en-US" />
+                    </div>
+                    <div className="flex items-start space-y-2 pt-1 flex-col">
+                      <div className="flex items-center">
+                        <Avatar className="w-5 h-5 mr-1">
+                          <AvatarImage
+                            src={
+                              video.uploader.avatarUrl
+                                ? video.uploader.avatarUrl
+                                : 'https://avatar.vercel.sh/personal.png'
+                            }
+                          />
+                          <AvatarFallback />
+                        </Avatar>
+                        <p className="w-8">{video.uploader.firstName}</p>
+                      </div>
+                      <div className="flex">
+                        {video.videoStatus === 'preparing' && (
+                          <Badge>Preparing</Badge>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+              </div>
             )
           })}
         {allAssets.length === 0 && (
@@ -78,98 +162,6 @@ export default async function Assets() {
           </p>
         )}
       </div>
-    </div>
-  )
-}
-
-function VideoRow({
-  video,
-  userImageUrl,
-  userName,
-}: {
-  video: SelectVideo
-  userImageUrl: string | undefined
-  userName: string
-}) {
-  return (
-    <div className="flex space-x-2 cursor-pointer">
-      <Link
-        href={`assets/${video.assetId}`}
-        className="flex flex-col md:flex-row md:space-x-2"
-      >
-        {video.videoStatus === 'ready' ? (
-          <div className="relative">
-            <Image
-              priority={true}
-              alt="video_thumbnail"
-              src={`https://image.mux.com/${video.playbackUrl}/thumbnail.jpg?width=300&fit_mode=pad`}
-              className="flex rounded-md"
-              width={279}
-              height={157}
-            />
-            <Badge className="font-bold text-xs absolute bottom-2 right-2 bg-primary/80">
-              {new Date(video.duration! * 1000).toISOString().slice(11, 19)}
-            </Badge>
-            <Badge className="absolute top-2 right-2 bg-primary/80">
-              {video.videoTypeEnum}
-            </Badge>
-          </div>
-        ) : (
-          <div className="bg-gray-400 w-[279px] h-[157px] rounded-md" />
-        )}
-        <div className="flex flex-col md:w-[400px]">
-          <div className="flex space-x-4 items-center justify-between">
-            <p className="lg:text-xl font-semibold">{video.videoName}</p>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="ghost">
-                  <span className="sr-only">Actions</span>
-                  <DotsHorizontalIcon className="h-4 w-4" />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end">
-                <DropdownMenuItem className="space-x-2">
-                  <ShareIcon className="w-4 h-4" />
-                  <p>Share</p>
-                </DropdownMenuItem>
-                <DropdownMenuItem className="space-x-2">
-                  <DownloadIcon className="h4 w-4" />
-                  <p>Download Video</p>
-                </DropdownMenuItem>
-                <DropdownMenuItem className="space-x-2">
-                  <ScissorsIcon className="h-4 w-4" />
-                  <p>Clip</p>
-                </DropdownMenuItem>
-                <DropdownMenuItem className="space-x-2">
-                  <BookOpenIcon className="h-4 w-4" />
-                  <p>Match Info</p>
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </div>
-          <div className="text-muted-foreground">
-            <TimeAgoClient date={video.createdAt} locale="en-US" />
-          </div>
-          <div className="flex items-start space-y-2 pt-1 flex-col">
-            <div className="flex items-center">
-              <Avatar className="w-5 h-5 mr-1">
-                <AvatarImage
-                  src={
-                    userImageUrl
-                      ? userImageUrl
-                      : 'https://avatar.vercel.sh/personal.png'
-                  }
-                />
-                <AvatarFallback />
-              </Avatar>
-              <p className="w-8">{userName}</p>
-            </div>
-            <div className="flex">
-              {video.videoStatus === 'preparing' && <Badge>Preparing</Badge>}
-            </div>
-          </div>
-        </div>
-      </Link>
     </div>
   )
 }
